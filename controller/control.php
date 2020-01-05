2002d196e7146dbcdf8dd268ff961c3839bda7de
ralphi2811 <ralphi2811@gmail.com>
2020-01-05 15:06:38 +0400

    Migrate funtion to control.php from frontend.php

--- controller/control.php
+++ controller/control.php
@@ -1,3 +1,8 @@
 <?php
-
+function hashPassword($password)
+{
+    $salage = 'oCpFstackP3';
+    $salted_password = "$password$salage";
+    return hash('sha256', $salted_password);
+}
 
--- controller/frontend.php
+++ controller/frontend.php
@@ -1,17 +1,1 @@
 <?php
-function userSession()
-{
-    if ($_SESSION['userID'] > 0) {
-        echo 'User identifié';
-    }
-    else {
-        echo 'redirection page login';
-    }
-}
-
-function hashPassword($password)
-{
-    $salage = 'oCpFstackP3';
-    $salted_password = "$password$salage";
-    return hash('sha256', $salted_password);
-}
