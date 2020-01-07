16e7d0a50343e43175f5f4828980d5c72bdbd450
ralphi2811 <ralphi2811@gmail.com>
2020-01-07 08:48:02 +0400

    Create createAccount View

--- view/frontend/createAccountView.php
+++ view/frontend/createAccountView.php
@@ -1,28 +1,25 @@
-<?php $title = 'Login'; ?>
+<?php $title = 'Créer un compte'; ?>
 
 <?php ob_start(); ?>
-<div class="login">
-    <h1>GBAF Login</h1>
-    <img src="../../public/images/LOGO_GBAF.png" alt="LOGO_GBAF" class="logo"/>
-    <div class="loginForm">
-        <form action="index.php?action=login" method="post">
-            <div>
-                <input type="text" id="username" name="username" placeholder="Nom d'utilisateur">
-            </div>
-            <div>
-                <input type="password" id="password" name="password" placeholder="Mot de passe">
-            </div>
-            <div>
-                <input class="button" type="submit" value="S'identifier">
-            </div>
-            
+<div class="login-page">
+    <div class="form">
+        <form class="login-form">
+            <input type="text" name="surname" placeholder="Nom"/>
+            <input type="text" name="name" placeholder="Prénom"/>
+            <input type="text" name="username" placeholder="Login"/>
+            <input type="password" name="password" placeholder="Mot de passe"/>
+            <input type="password" name="password_check" placeholder="Vérifier mot de passe"/>
+            <select name="question">
+                <option value="">--Choisissez--</option>
+                <option value="animal">Nom de votre animal</option>
+                <option value="mother">Nom de jeune fille</option>
+                <option value="car">Première voiture</option>
+            </select>
+            <input type="text" name="answer" placeholder="Réponse"/>
+            <input class="button" type="submit" value="créer un compte">
+            <p class="message">Pas de compte ? <a href="index.php?action=register">Créer un compte</a></p>
+            <p class="message">Mot de passe oublié ? <a href="index.php?action=lostpassword">Réinitialiser</a></p>
         </form>
-        <div>
-            <a href="index.php?action=lostpassword">Mot de passe oublié ?</a>
-        </div>
-        <div>
-            <a href="index.php?action=createaccount">Créer un compte</a>
-        </div>
     </div>
 </div>
 
