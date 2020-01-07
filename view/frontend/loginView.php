c3608b19de182b3621548836fc81ddba46f8eb2b
ralphi2811 <ralphi2811@gmail.com>
2020-01-07 08:04:40 +0400

    Fix loginView.php

--- view/frontend/loginView.php
+++ view/frontend/loginView.php
@@ -5,8 +5,8 @@
     <img src="../../public/images/LOGO_GBAF.png" alt="Logo GBAF">
     <div class="form">
         <form class="login-form">
-        <input type="text" placeholder="Login"/>
-        <input type="password" placeholder="Mot de passe"/>
+            <input type="text" name="username" placeholder="Login"/>
+            <input type="password" name="password" placeholder="Mot de passe"/>
          <input class="button" type="submit" value="LOGIN">
         <p class="message">Pas de compte ? <a href="index.php?action=register">Créer un compte</a></p>
         <p class="message">Mot de passe oublié ? <a href="index.php?action=lostpassword">Réinitialiser</a></p>
