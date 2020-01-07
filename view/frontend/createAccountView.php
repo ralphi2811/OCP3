a4d96c78c57508f17251ec1477d7af1e5109fe57
ralphi2811 <ralphi2811@gmail.com>
2020-01-07 09:11:40 +0400

    Minor fix in createAccountView

--- view/frontend/createAccountView.php
+++ view/frontend/createAccountView.php
@@ -10,10 +10,10 @@
             <input type="password" name="password" placeholder="Mot de passe"/>
             <input type="password" name="password_check" placeholder="Vérifier mot de passe"/>
             <select name="question">
-                <option value="">--Choisissez--</option>
+                <option value="">-- Question Secrète --</option>
                 <option value="animal">Nom de votre animal</option>
-                <option value="mother">Nom de jeune fille</option>
-                <option value="car">Première voiture</option>
+                <option value="mother">Nom de jeune fille de votre mère</option>
+                <option value="car">Votre première voiture</option>
             </select>
             <input type="text" name="answer" placeholder="Réponse"/>
             <input class="button" type="submit" value="créer un compte">
