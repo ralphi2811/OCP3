5564b1778d3523a713c2cc3c19724fac280300e6
ralphi2811 <ralphi2811@gmail.com>
2020-01-10 11:00:21 +0400

    errorViex & css modification

--- view/frontend/errorView.php
+++ view/frontend/errorView.php
@@ -1,9 +1,13 @@
 <?php $title = 'Erreur'; ?>
 
 <?php ob_start(); ?>
-<div class="errormessage">
-    <p><?= $errorMessage ?></p> 
-</div>
+    <div class="errormessage">
+        <img class="login_logo" src="../../public/images/LOGO_GBAF.png" alt="Logo GBAF">
+        <div class="error_message">
+            <p><?= $errorMessage ?></p> 
+            <a href="index.php">Retour à l'accueil</a>
+        </div>    
+    </div>
 
 <?php $content = ob_get_clean(); ?>
 
