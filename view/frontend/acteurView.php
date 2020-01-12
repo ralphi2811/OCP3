d6299aff479a8f85df7199bcbd6874323d946e16
ralphi2811 <ralphi2811@gmail.com>
2020-01-12 17:11:19 +0400

    Fix header & footer include

--- view/frontend/acteurView.php
+++ view/frontend/acteurView.php
@@ -1,7 +1,7 @@
 <?php $title = 'GBAF - Acceuil'; ?>
 
 <?php ob_start(); ?>
-<?= require_once 'view/frontend/header.php'; ?>
+<?php include("header.php"); ?>
 
 <div class="gbaf-container">
     
@@ -12,7 +12,7 @@
     
 </div>
 
-<?= require_once 'view/frontend/footer.php'; ?>
+<?php include("footer.php"); ?>
 <?php $content = ob_get_clean(); ?>
 
 <?php require 'template.php'; ?>
