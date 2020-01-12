b6f699eb1e679cbf153f245ec646193399cbd562
ralphi2811 <ralphi2811@gmail.com>
2020-01-12 12:16:50 +0400

    Fix header

--- view/frontend/header.php
+++ view/frontend/header.php
@@ -1,10 +1,9 @@
 <header>
-    <div class='header-container'>
-    <img class="login_logo" src="../../public/images/LOGO_GBAF.png" alt="Logo GBAF">
-        <div class="header-user">
-            <i class="fas fa-user"></i>
+    <div class='header'>
+        <img class="logo" src="../../public/images/LOGO_GBAF.png" alt="Logo GBAF">
+        <div class="header-right">            
             <a href="index.php?action=myaccount"><?= $_SESSION['surname'] ?> <?= $_SESSION['name'] ?></a>
             <a href="index.php?action=logout">Se déconnecter</a>
-        </div>
+         </div>
     </div>
 </header>
