094fd5129a9b9358b4a6ddada413ab65c776f198
ralphi2811 <ralphi2811@gmail.com>
2020-01-14 12:04:40 +0400

    Change ol 2 ul in footer file

--- view/frontend/footer.php
+++ view/frontend/footer.php
@@ -1,8 +1,8 @@
 <footer>
     <div class="footer">
-        <ol>
+        <ul>
             <li><a href="index.php?action=contact">Contact</a></li>
             <li><a href="index.php?action=legal">Mentions légales</a></li>
-        </ol>
+        </ul>
     </div>
 </footer>
