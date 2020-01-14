7a000cba95deca9ce40f7e897c72cb4c10736bc4
ralphi2811 <ralphi2811@gmail.com>
2020-01-14 14:00:11 +0400

    Clean php files

--- model/frontend.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'config/database.php'; // PDO  BDD avec la fondction dbconnect()
-
-function signUp($nom,$prenom,$username,$password,$question,$reponse)
-{
-    $db = dbconnect(); //connection bdd
-    
-}
