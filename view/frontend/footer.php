65fbb62abf2e50f4e3ee994bfcccae727a9a2bc1
ralphi2811 <ralphi2811@gmail.com>
2020-01-14 12:05:16 +0400

    Add class for Css in footer

--- view/frontend/footer.php
+++ view/frontend/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer">
-        <ul>
+        <ul class="inline">
             <li><a href="index.php?action=contact">Contact</a></li>
             <li><a href="index.php?action=legal">Mentions légales</a></li>
         </ul>
