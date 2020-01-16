97b8a912ef04dfb9ba991da9f79f099c4339194a
ralphi2811 <ralphi2811@gmail.com>
2020-01-16 09:26:03 +0400

    Clean console.log for production

--- public/js/snackbar.js
+++ public/js/snackbar.js
@@ -1,7 +1,5 @@
 var divSnackContetnt = document.getElementById("snackbar").innerHTML
 if (divSnackContetnt !== '') {
-    console.log('Ok contenu récupéré')
-    console.log(divSnackContetnt)
     var x = document.getElementById("snackbar");
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);   
