108893aa3c2c85f044931edbb8cda657b7e1faf8
ralphi2811 <ralphi2811@gmail.com>
2020-01-17 17:59:55 +0400

    Start function in ActeurManager

--- model/ActeurManager.php
+++ model/ActeurManager.php
@@ -1,2 +1,12 @@
 <?php
+
 namespace Sixkreation\Ocp3\Model;
+
+require_once 'model/Manager.php';
+
+class ActeurManager extends Manager {
+    public function getActors() {
+        $db = $this->dbConnect();
+        $req = $db->query($statement); ///////////////////////////////// FINIR ICI
+    }
+}
