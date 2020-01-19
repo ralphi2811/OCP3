695fd891ebdc511390db6adde44c3886735c53e5
ralphi2811 <ralphi2811@gmail.com>
2020-01-19 15:06:40 +0400

    Update request for first line import from DB

--- model/ActeurManager.php
+++ model/ActeurManager.php
@@ -7,13 +7,17 @@ require_once 'model/Manager.php';
 class ActeurManager extends Manager {
     public function getActors() {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT `id_acteur`, SUBSTR(description,1,200) AS description,`acteur`,`logo` FROM `acteur`'); 
+        $req = $db->query("SELECT `id_acteur`, SUBSTRING_INDEX(description,'\n',1) AS description,`acteur`,`logo` FROM `acteur`"); 
         
         return $req;
     }
     
     public function getActor($id_acteur) {
         $db = $this->dbConnect();
-        $req = $db->query($statement);
+        $req = $db->query('SELECT * FROM `acteur` WHERE `id_acteur` =?');
+        $req->execute(array($id_acteur));
+        $actor = $req->fetch();
+        
+        return $actor;
     }
 }
