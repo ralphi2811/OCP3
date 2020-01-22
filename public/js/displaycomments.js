ce0628e0dc3567e9d9b29664afcc1a06bdb44497
ralphi2811 <ralphi2811@gmail.com>
2020-01-22 16:58:41 +0400

    Add Css animation for new comment

--- public/js/displaycomments.js
+++ public/js/displaycomments.js
@@ -1,5 +1,14 @@
+var visible = 0;
 function displaycomments() {
-    document.getElementById("hidden-div").style.display = "block";
+    if (visible === 0) {
+        document.getElementById("hidden-div").style.display = "block";
+        visible = 1;
+    }
+    else {
+        document.getElementById("hidden-div").style.display = "none";
+        visible = 0;
+    }
+  
 }
 
 
