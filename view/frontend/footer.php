f6dfe16f4b1ec788d2209277f8fbc6adce1bec0c
ralphi2811 <ralphi2811@gmail.com>
2020-01-24 15:16:48 +0400

    Minor FontAwesome updates

--- view/frontend/footer.php
+++ view/frontend/footer.php
@@ -1,8 +1,8 @@
 <footer>
     <div class="footer">
         <ul class="inline">
-            <li><a href="index.php?action=contact">Contact</a></li>
-            <li><a href="index.php?action=legal">Mentions légales</a></li>
+            <li><a href="index.php?action=contact"><i class="fas fa-envelope"></i> Contact</a></li>
+            <li><a href="index.php?action=legal"><i class="fas fa-balance-scale"></i> Mentions légales</a></li>
         </ul>
     </div>
 </footer>
--- view/frontend/header.php
+++ view/frontend/header.php
@@ -3,8 +3,8 @@
         <a href="index.php?action=actors"><img class="logo" src="../../public/images/LOGO_GBAF.png" alt="Logo GBAF"></a>
         <div class="header-right">
             <i class="far fa-user-circle"></i>
-            <a href="index.php?action=myaccount"><i class="fas fa-pen"></i> <?= $_SESSION['surname'] ?> <?= $_SESSION['name'] ?></a>
-            <a href="index.php?action=logout">Se déconnecter</a>
+            <a href="index.php?action=myaccount"><i class="fas fa-user-edit"></i> <?= $_SESSION['surname'] ?> <?= $_SESSION['name'] ?></a>
+            <a href="index.php?action=logout"><i class="fas fa-sign-out-alt"></i> Se déconnecter</a>
          </div>
     </div>
 </header>
