6b10db49ed0e3944dc9629d42dd5eaa7fedec06c
ralphi2811 <ralphi2811@gmail.com>
2020-01-28 10:54:02 +0400

    Minor Update

--- view/frontend/legalView.php
+++ view/frontend/legalView.php
@@ -1,20 +1,31 @@
-<?php $title = 'Mentions légales'; ?>
+<?php $title = 'GBAF - Mentions légales'; ?>
 
 <?php ob_start(); ?>
 
 <?php include("header.php"); ?>
 
 <div class="gbaf-container">
-    <div class="legal">
-        <p>Site hébergé chez OVH SAS<br></p>
-        <p><strong>Infos Société :</strong></p>
-        <p>GBAF Groupement des Banques et Assurances Francaises <br>73120 Saint-Bon-Tarentaise <br>Tel. 0123 456 789<br>SIRET 777 888 999 00099<br>immatriculé au registre du commerce et des sociétés de CHAMBERI</p>
-        <p><strong>Crédit photo :</strong></p>
-        <p><br>GBAF / OPENCLASSROOMS</p>
-        <p><strong>Développeur :</strong></p>
-        <p> Raphaël Auberlet<br>+262 693 395 898 </p>
-    </div>
-   
+    <div class="login-page">
+        <h2>Mentions légales</h2>
+        <div class="form">
+            <p><h3>Hébergeur :</h3></p>
+            <p>Site hébergé chez OVH SAS<br></p>
+            <p><h3>Projet :</h3></p>
+            <p>Ce site a été réalisé dans le cadre du projet 3 du parcour Prep'fullstack sur OPENCLASSROOMS<br></p>
+            <p><h3>Infos Société :</h3></p>
+            <p>
+                GBAF Groupement des Banques et Assurances Francaises <br>
+                75000 Paris <br>
+                Tel. 0123 456 789<br>
+                SIRET 777 888 999 00099<br>
+                immatriculé au registre du commerce et des sociétés de XXXX
+            </p>
+            <p><h3>Crédit photo :</h3></p>
+            <p><br>OPENCLASSROOMS<br>PIXABAY</p>
+            <p><h3>Développeur :</h3></p>
+            <p> Raphaël Auberlet<br>+262 693 395 898 </p>
+        </div>
+    </div>    
 </div>
 
 <?php include("footer.php"); ?>
